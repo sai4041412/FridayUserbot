020812f0074e95a28f4a8e57a53e9ec1ae55bd09
Restyled.io <commits@restyled.io>
2020-11-07 15:04:57 +0000

    Restyled by yapf

--- fridaybot/modules/afk.py
+++ fridaybot/modules/afk.py
@@ -21,9 +21,7 @@ last_afk_message = {}
 afk_start = {}
 
 
-@friday.on(
-    events.NewMessage(pattern=r"\.afk ?(.*)", outgoing=True)
-)  # pylint:disable=E0602
+@friday.on(events.NewMessage(pattern=r"\.afk ?(.*)", outgoing=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -42,17 +40,18 @@ async def _(event):
     reason = event.pattern_match.group(1)
     if not USER_AFK:  # pylint:disable=E0602
         last_seen_status = await borg(  # pylint:disable=E0602
-            functions.account.GetPrivacyRequest(types.InputPrivacyKeyStatusTimestamp())
-        )
+            functions.account.GetPrivacyRequest(
+                types.InputPrivacyKeyStatusTimestamp()))
         if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
             await borg.send_message(
-                event.chat_id, f"**I am busy now 👀.** \n__He went offline__ `{reason}`"
-            )
+                event.chat_id,
+                f"**I am busy now 👀.** \n__He went offline__ `{reason}`")
         else:
-            await borg.send_message(event.chat_id, f"**I Am Busy And I Am Going Afk**.")
+            await borg.send_message(event.chat_id,
+                                    f"**I Am Busy And I Am Going Afk**.")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -80,8 +79,7 @@ async def set_not_afk(event):
         shite = await borg.send_message(
             event.chat_id,
             "__I am back__\n**No Longer offline.**\n `I Went offline with afk for:``"
-            + total_afk_time
-            + "`",
+            + total_afk_time + "`",
         )
         try:
             await borg.send_message(  # pylint:disable=E0602
@@ -91,9 +89,9 @@ async def set_not_afk(event):
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
                 event.chat_id,
-                "Please set `PRIVATE_GROUP_ID` "
-                + "for the proper functioning of afk functionality "
-                + "Please Seek Support in @FridayOT\n\n `{}`".format(str(e)),
+                "Please set `PRIVATE_GROUP_ID` " +
+                "for the proper functioning of afk functionality " +
+                "Please Seek Support in @FridayOT\n\n `{}`".format(str(e)),
                 reply_to=event.message.id,
                 silent=True,
             )
@@ -105,9 +103,8 @@ async def set_not_afk(event):
 
 @friday.on(
     events.NewMessage(  # pylint:disable=E0602
-        incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
-    )
-)
+        incoming=True,
+        func=lambda e: bool(e.mentioned or e.is_private)))
 async def on_afk(event):
     if event.fwd_from:
         return
@@ -143,8 +140,7 @@ async def on_afk(event):
             elif days > 1:
                 if days > 6:
                     date = now + datetime.timedelta(
-                        days=-days, hours=-hours, minutes=-minutes
-                    )
+                        days=-days, hours=-hours, minutes=-minutes)
                     afk_since = date.strftime("%A, %Y %B %m, %H:%I")
                 else:
                     wday = now + datetime.timedelta(days=-days)
@@ -158,9 +154,8 @@ async def on_afk(event):
         msg = None
         message_to_reply = (
             f"**My Boss is Afk**  \n**AFKT** : `{total_afk_time}`\n**Reason** : `{reason}`"
-            + f"\n\nHe Will Reply To You Soon!"
-            if reason
-            else f"**My Master is Afk**\n AFK : `{total_afk_time}` He Will Comeback Soon"
+            + f"\n\nHe Will Reply To You Soon!" if reason else
+            f"**My Master is Afk**\n AFK : `{total_afk_time}` He Will Comeback Soon"
         )
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
